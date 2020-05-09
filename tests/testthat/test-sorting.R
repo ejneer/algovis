818cbf336dde69cb4ba0617b5562aea15f1d7dbb
Eric Neer <ericjneer@gmail.com>
2020-05-08 21:02:58 -0700

    remove reliance on random function

--- tests/testthat/test-sorting.R
+++ tests/testthat/test-sorting.R
@@ -1,11 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
-
 test_that("sorting works", {
-  test_vector <- runif(10) * 10
+  test_vector <-
+    c(
+      7.095809,
+      2.825275,
+      1.128111,
+      6.599878,
+      4.230301,
+      2.535363,
+      4.921699,
+      2.825585,
+      3.312714,
+      3.237771
+    )
   sorted_vector <- sort(test_vector)
-  mergeSort(test_vector)
+  test_vector <- mergeSort(test_vector, snapshot = FALSE)
   
   expect_equal(test_vector, sorted_vector)
 })
